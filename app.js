4b852dfa9e7479fdc39622d9e56d38bcd86d2495
D3nn1S-Mugart3 <dennis.mugarte05@gmail.com>
2024-07-11 20:56:17 -0600

    api de Yahir

--- app.js
+++ app.js
@@ -1,44 +1,45 @@
 const express = require("express");
 const cors = require("cors");
 const { swaggerDocs: V1SwaggerDocs } = require("./src/swaggers");
-const bodyParser = require('body-parser');
-require('dotenv').config();
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const eventRoutes = require('./routes/eventRoutes');
-const subirImagen = require('./routes/imagenRoutes'); // Corrige la ruta relativa
-const authMiddleware = require('./middleware/authMiddleware');
-const sequelize = require('./config/db');
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const eventRoutes = require("./routes/eventRoutes");
+const subirImagen = require("./routes/imagenRoutes"); // Corrige la ruta relativa
+const authMiddleware = require("./middleware/authMiddleware");
+const sequelize = require("./config/db");
+const ENV = require("./src/config/config");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = ENV.port || 3000;
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
 //teos
-app.use("/api", require('./src/api_routes'));
+app.use("/api", require("./src/api_routes"));
 
 //rutas - irvings
-app.use('/api/imagenes', subirImagen); // Utiliza imagenesRouter para las rutas de imágeness
-app.use('/api', authRoutes);
-app.use('/api', userRoutes);
-app.use('/api/events', eventRoutes);
+app.use("/api/imagenes", subirImagen); // Utiliza imagenesRouter para las rutas de imágeness
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api/events", eventRoutes);
 
 // Ruta protegida por middleware de autenticación
-app.get('/dashboard', authMiddleware, (req, res) => {
-    res.json({ message: `Welcome ${req.userRole}` });
-  });
-
-
-sequelize.sync().then(() => {
-    app.listen(PORT, () => {
-    console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
-    V1SwaggerDocs(app, PORT);
+app.get("/dashboard", authMiddleware, (req, res) => {
+  res.json({ message: `Welcome ${req.userRole}` });
 });
-}).catch(err => {
-    console.error('Error al sincronizar la base de datos:', err);
-});  
 
-
+sequelize
+  .sync()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
+      V1SwaggerDocs(app, PORT);
+    });
+  })
+  .catch((err) => {
+    console.error("Error al sincronizar la base de datos:", err);
+  });
