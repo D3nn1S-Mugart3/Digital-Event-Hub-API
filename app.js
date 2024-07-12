4382ca4b8d3c434920ee8f6ffef48e8ceaf5d97c
Christian Navarro <christianyahirnavarrochan@gmail.com>
2024-07-11 20:22:49 -0600

    agregando api de irving

--- app.js
+++ app.js
@@ -1,15 +1,44 @@
 const express = require("express");
 const cors = require("cors");
 const { swaggerDocs: V1SwaggerDocs } = require("./src/swaggers");
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+const eventRoutes = require('./routes/eventRoutes');
+const subirImagen = require('./routes/imagenRoutes'); // Corrige la ruta relativa
+const authMiddleware = require('./middleware/authMiddleware');
+const sequelize = require('./config/db');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json());
+
+//teos
 app.use("/api", require('./src/api_routes'));
 
-app.listen(PORT, () => {
+//rutas - irvings
+app.use('/api/imagenes', subirImagen); // Utiliza imagenesRouter para las rutas de imágeness
+app.use('/api', authRoutes);
+app.use('/api', userRoutes);
+app.use('/api/events', eventRoutes);
+
+// Ruta protegida por middleware de autenticación
+app.get('/dashboard', authMiddleware, (req, res) => {
+    res.json({ message: `Welcome ${req.userRole}` });
+  });
+
+
+sequelize.sync().then(() => {
+    app.listen(PORT, () => {
     console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
     V1SwaggerDocs(app, PORT);
 });
+}).catch(err => {
+    console.error('Error al sincronizar la base de datos:', err);
+});  
+
+
